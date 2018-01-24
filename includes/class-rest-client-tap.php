72a58be2e59a40f2c69349f04e7033259acabfc8
Mr. Brazzi <luka.ghost@gmail.com>
2018-01-23 21:42:40 -0500

    = 1.0.1 = * Added unique function names in main plugin file.

--- includes/class-rest-client-tap.php
+++ includes/class-rest-client-tap.php
@@ -69,7 +69,7 @@ class RestClientTap {
 	public function __construct() {
 
 		$this->plugin_name = 'rest-client-tap';
-		$this->version = '1.0.0';
+		$this->version = '1.0.1';
 
 		$this->load_dependencies();
 		$this->set_locale();
